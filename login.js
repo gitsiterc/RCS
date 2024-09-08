896081aa9baa6671bcb5cf271904aafd722d52b3
gitsiterc <ploetzlich.idiot@gmail.com>
2024-09-08 21:13:01 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+const users = {
+    '123': '123',
+    'user2': 'password2',
+    'user3': 'password3'
+};
+
+function login(username, password) {
+    if (users[username] && users[username] === password) {
+        localStorage.setItem('loggedInUser', username);
+        return true;
+    }
+    return false;
+}
+// Login-Formular-Event-Listener
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (users[username] && users[username] === password) {
+        // Speichern des Login-Status im localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'home.html';
+    } else {
+        alert('Ungültiger Benutzername oder Passwort');
+    }
+});
---
+++ script.js
@@ -0,0 +1,32 @@
+function checkLoginStatus() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+    // Wenn der Nutzer nicht angemeldet ist, weiterleiten zu index.html
+    if (!isLoggedIn) {
+        window.location.href = 'index.html';
+    }
+}
+
+// Diese Funktion wird beim Laden der Seite aufgerufen
+window.onload = checkLoginStatus;
+
+document.getElementById('logoutButton').addEventListener('click', function() {
+    // Entfernen des Login-Status aus dem localStorage
+    localStorage.removeItem('isLoggedIn');
+    // Weiterleiten zur Login-Seite
+    window.location.href = 'index.html';
+});
+
+
+// Diese Funktion wird beim Laden der Seite aufgerufen
+window.onload = checkLoginStatus;
+document.getElementById('popupButton').addEventListener('click', function() {
+    var popup = document.getElementById('popup');
+    if (popup.style.display === 'block') {
+        popup.style.display = 'none';
+    } else {
+        popup.style.display = 'block';
+    }
+});
+
+
